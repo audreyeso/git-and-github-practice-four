121d26c9236276a9876a6e25b661ba17cf24786e
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-06-07 18:37:44 -0700

    practice four

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int one = 1;
+        int two = 2;
+        int three = 3;
+
+        String string1, string2, string3;
+
+        string1 = "California";
+        string2 = "Washington";
+        string3 = "Utah";
+
+    }
+}
